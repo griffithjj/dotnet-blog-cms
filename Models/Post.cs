8e498aeb6216c52ba27c3eff8a66564fd872bc1c
Juergen Loff <jj.loff@gmail.com>
2024-08-22 22:17:15 +0200

    First commit

---
+++ Models/Post.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlogCms.Models;
+
+public class Post(string title = "", string summary = "", string content = "")
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = title;
+    public string Summary { get; set; } = summary;
+    public string Content { get; set; } = content;
+    [DataType(DataType.Date)]
+    public DateTime Created { get; set; } = DateTime.Now;
+    [DataType(DataType.Date)]
+    public DateTime LastUpdated { get; set; } = DateTime.Now;
+}
